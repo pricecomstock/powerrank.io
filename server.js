eaddd0345a71e0fb698dad3ed5b4ac7240c22772
Patrick Davlin <pdavasaurusrex@gmail.com>
2017-09-13 18:16:59 -0500

    tweaking server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // app.get('/', function (req, res) {
 //   res.send('Hello World!')
@@ -9,6 +9,7 @@ const app = express()
 //   console.log('Example app listening on port 3000!')
 // })
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+app.listen(8080);
