da3a14528e23891deb9fd441d2b6f22cfa08e7f1
Patrick Davlin <pdavlin@hayneedle.com>
2017-09-13 16:36:21 -0500

    initial commit + express dependency + server.js re-added

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
