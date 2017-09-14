38cfff223568cea1a3ef50cb5aa179f5a432a76a
Patrick Davlin <pdavasaurusrex@gmail.com>
2017-09-13 19:06:30 -0500

    server.js will now just serve static content from root

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
+app = express();
+app.use(serveStatic(__dirname));
 
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!')
-// })
+var port = process.env.PORT || 5000;
+app.listen(port);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-app.listen(8080);
+console.log('server started '+ port);
