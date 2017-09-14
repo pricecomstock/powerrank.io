ec19e66570d727489983dcff3037976e0f968c2c
Patrick Davlin <pdavasaurusrex@gmail.com>
2017-09-14 17:46:03 -0500

    add semicolons back in

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const serveStatic = require('serve-static')
+const express = require('express');
+const serveStatic = require('serve-static');
 
-const app = express()
-app.use(serveStatic(__dirname))
+const app = express();
+app.use(serveStatic(__dirname));
 
-const port = process.env.PORT || 5000
-app.listen(port)
+const port = process.env.PORT || 5000;
+app.listen(port);
 
-console.log('server started ' + port)
+console.log('server started ' + port);
