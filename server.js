ec3463ab664f515f942f953b8c1ec2a122b56350
Patrick Davlin <pdavasaurusrex@gmail.com>
2017-10-31 02:28:56 +0000

    Revert "Develop (pull request #10)"
    This reverts pull request #10.
    
    > * added support for eslint and standardized editor configuration
    > 
    > * more package.json cleanup
    > 
    > * some ES6 optimization, eslint rules, and package management.
    > 
    > * require semicolons
    > 
    > * add semicolons back in
    > 
    > * package fixing, please
    > 
    > * implemented eslint in webpack; isn't checking prior to webpack build or dev
    > 
    > * added linting for js in .vue files, fixed initial errors in vue files - everything up to standards.
    > 
    > * Pulled lists out into components, but page is non-functional now
    > 
    > * this is just as broken as before but we've made some steps in the right direction, i'll make a branch next time pdav i swear
    > 
    > * I think this will be the last broken commit. Dragging works intra-list now
    > 
    > * Cleaned up commented code. Restored functionality

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const serveStatic = require('serve-static');
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
 
-const app = express();
+app = express();
 app.use(serveStatic(__dirname));
 
-const port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('server started ' + port);
+console.log('server started '+ port);
