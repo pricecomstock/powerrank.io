5c04858cf84bd2e7ba2c9eb608c9af2a6b51724b
Price Comstock <price.comstock@gmail.com>
2018-03-27 18:46:09 -0500

    fixed ranking view button

--- src/components/pages/ranking/RankSuccess.vue
+++ src/components/pages/ranking/RankSuccess.vue
@@ -7,13 +7,13 @@
 						You have successfully powerranked <strong>{{this.$store.getters.rankList.title}}</strong>.
 					</h1>
 					<p class="field">
-						<router-link :to="rankPath" class="button is-info is-large" tag="a">
+						<router-link :to="rankingLink" class="button is-info is-large" tag="a">
 							<span class="icon is-small"><i class="fas fa-list"></i></span>
 							<span>View Ranking</span>
 						</router-link>
 					</p>
 					<p class="field">
-						<router-link :to="rankResultsPath" class="button is-info is-large" tag="a">
+						<router-link :to="resultsLink" class="button is-info is-large" tag="a">
 							<span class="icon is-small"><i class="fas fa-trophy"></i></span>
 							<span>View Results</span>
 						</router-link>
@@ -30,33 +30,25 @@
 		components: {
 		},
 		props: {
-			id: {
+			rankListId: {
 				type: String,
 				required: true
 			}
 		},
 		data() {
 			return {
-				rankPath: '',
-				rankResultsPath: ''
 			};
 		},
 		computed: {
 			rankingId() {
 				return this.$store.getters.createdRankId;
 			},
-			link() {
-				const routeBase = window.location.href.substr(0, window.location.href.indexOf('/rank'))
-				return routeBase + this.rankPath
+			rankingLink() {
+				return '/ranking/' + this.rankingId
 			},
 			resultsLink() {
-				const routeBase = window.location.href.substr(0, window.location.href.indexOf('/rank'))
-				return routeBase + this.rankResultsPath
+				return '/ranking/' + this.rankListId
 			}
-		},
-		created() {
-			this.rankPath = '/ranking/' + this.rankingId
-			this.rankResultsPath = '/rankresults/' + this.id
 		}
 	};
 </script>
--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,7 @@ export const routes = [
 	{ path: '/create/success', component: CreationSuccess },
 	{ path: '/browse', component: Browse },
 	{ path: '/rank/:id', component: PowerRank, props: true },
-	{ path: '/rank/:id/success', component: RankSuccess, props: true },
+	{ path: '/rank/:rankListId/success', component: RankSuccess, props: true },
 	{ path: '/ranking/:rankingId', component: ViewRanking, props: true },
 	{ path: '/rankresults/:id', component: RankResults, props: true }
 ];
