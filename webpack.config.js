90e40c80c4f65e30a6d48d55458654f5734b82a9
Price Comstock <price.comstock@gmail.com>
2019-04-27 10:49:37 -0500

    docker container works

--- webpack.config.js
+++ webpack.config.js
@@ -60,12 +60,12 @@ if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"',
-        __API_BASE_URL__: '"http://beta.powerrank.io/api/"'
-      }
-    }),
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     NODE_ENV: '"production"',
+    //     __API_BASE_URL__: '"api/"'
+    //   }
+    // }),
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
